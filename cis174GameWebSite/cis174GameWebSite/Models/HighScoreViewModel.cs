a003ed486a7d5f050385c257408a429bbdb136d2
Carl Phillips <36976465+carl-phillips@users.noreply.github.com>
2019-12-01 16:17:29 -0600

    Adding HighScore table
    Adding HighScore table to the database. Cretaed context

--- cis174GameWebSite/cis174GameWebSite/Models/HighScoreViewModel.cs
+++ cis174GameWebSite/cis174GameWebSite/Models/HighScoreViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace cis174GameWebSite.Models
 {
     public class HighScoreViewModel
     {
+        [Key]
         public int ScoreId { get; set; }
         public long Score { get; set; }
+        public int UserId { get; set; }
     }
 }
+ 
